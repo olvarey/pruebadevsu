09fb9d16c04d0035437717768a5242f6c243a8c5
Melvin Reyes <olva.reyes@gmail.com>
2026-04-12 07:55:29 -0600

    first commit

---
+++ src/main/java/com/devsu/pruebadevsu/PruebadevsuApplication.java
@@ -0,0 +1,13 @@
+package com.devsu.pruebadevsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebadevsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebadevsuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsu/pruebadevsu/PruebadevsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsu.pruebadevsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebadevsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
